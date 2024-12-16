0a53cedfd11381589c02744fab78b5e25d3c8d68
James Woodward <james.woodward@spaceapegames.com>
2024-12-16 14:58:06 +0000

    move the ICsvSerializer into CsvSerializer and add a comment

--- src/Game/Settings/CsvSerializer.h
+++ src/Game/Settings/CsvSerializer.h
@@ -1,5 +1,15 @@
 #pragma once
-#include "ICsvSerializer.h"
+
+// ICsvSerializer exists so that we can use gmock on the CsvSerializer in our tests
+template<typename SerializeType>
+class ICsvSerializer {
+public:
+    virtual ~ICsvSerializer() = default;
+    virtual SerializeType load(const std::string& configPath) = 0;
+    virtual SerializeType loadAsync(const std::string& configPath) = 0;
+    virtual void update(const SerializeType& serialize, const std::string& configPath) = 0;
+    virtual void updateAsync(const SerializeType& serialize, const std::string& configPath) = 0;
+};
 
 template <typename SerializeType>
 class CsvSerializer : public ICsvSerializer<SerializeType> {
@@ -9,5 +19,6 @@ public:
     void update(const SerializeType& serialize, const std::string& configPath) override;
     void updateAsync(const SerializeType& serialize, const std::string& configPath) override;
 };
+
 
 #include "CsvSerializer.inl"
